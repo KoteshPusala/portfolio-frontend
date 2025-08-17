7cf283ae56b7ca984405d8e5628d040ab84c3f6e
KoteshPusala <ugs23056_cse.kotesh@cbit.org.in>
2025-08-17 19:23:43 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -12,7 +12,7 @@ export default function Contact() {
     setLoading(true);
     setStatus(null);
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://portfolio-backend-vuui.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
